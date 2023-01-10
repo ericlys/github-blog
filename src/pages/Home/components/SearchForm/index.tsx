c16a02858abb89b8f3cbdeedf5a68403687953a3
Ericlys Moreira <ericlysm@gmail.com>
2023-01-09 23:48:04 -0300

    chore: search query

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,13 +1,31 @@
+import { useForm } from 'react-hook-form'
 import { SearchFormContainer } from './styles'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInput = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  const handleSearchPosts = (data: SearchFormInput) => {
+    console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <header>
         <h4>Publicações</h4>
         <span>6 publicações</span>
       </header>
-      <input placeholder="Buscar conteúdo" />
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
+      <input type="submit" hidden />
     </SearchFormContainer>
   )
 }
